ead7bc959fa768460e0c21fb7a5c11c60da602be
jinhyo <sosilion@gmail.com>
2021-07-13 17:26:18 +0900

    Morgan Module 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { MorganInterceptor, MorganModule } from 'nest-morgan';
 import { MyConfigModule } from './modules/my-config/my-config.module';
 
 @Module({
-  imports: [MyConfigModule],
+  imports: [MyConfigModule, MorganModule],
   controllers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: MorganInterceptor('dev'),
+    },
+  ],
 })
 export class AppModule {}
